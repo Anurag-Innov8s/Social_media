a51314b9f4f5528d676c20dc4e2854354f7f366a
Anurag Kumar <anuraganu202016@gmail.com>
2024-02-12 18:07:06 +0530

    fixed frontend

---
+++ frontend/src/Actions/User.js
@@ -0,0 +1,108 @@
+import axios from "axios";
+
+export const loginUser = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoginRequest",
+    });
+
+    const { data } = await axios.post(
+      "/login",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({
+      type: "LoginSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "LoginFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoadUserRequest",
+    });
+
+    const { data } = await axios.get("/me");
+
+    dispatch({
+      type: "LoadUserSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "LoadUserFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getFollowingPosts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "postOfFollowingRequest",
+    });
+
+    const { data } = await axios.get("/posts");
+    dispatch({
+      type: "postOfFollowingSuccess",
+      payload: data.posts,
+    });
+  } catch (error) {
+    dispatch({
+      type: "postOfFollowingFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getMyPosts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "myPostsRequest",
+    });
+
+    const { data } = await axios.get("/my/posts");
+    dispatch({
+      type: "myPostsSuccess",
+      payload: data.posts,
+    });
+  } catch (error) {
+    dispatch({
+      type: "myPostsFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getAllUsers =
+  (name = "") =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: "allUsersRequest",
+      });
+
+      const { data } = await axios.get(`/users?name=${name}`);
+      dispatch({
+        type: "allUsersSuccess",
+        payload: data.users,
+      });
+    } catch (error) {
+      dispatch({
+        type: "allUsersFailure",
+        payload: error.response.data.message,
+      });
+    }
+  };

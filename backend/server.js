565b664e939515e59effe1fc4e23261e17b8cc32
Anurag Kumar <anuraganu202016@gmail.com>
2024-02-28 18:36:53 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
 const { connectDatabase } = require("./config/database");
 const cloudinary = require('cloudinary')
+const port = process.env.PORT || 5000;
+const path = require("path")
+const express = require("express")
 connectDatabase()
 
 cloudinary.config({
@@ -8,9 +11,16 @@ cloudinary.config({
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
 })
-app.get("/", (req, res) => {
-    res.json({ message: "Hello I am Backend" });
-  });
+app.use(express.static(path.join(__dirname, "./frontend/build")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(
+        path.join(__dirname,"./frontend/build/index.html"),
+        function(err){
+            res.status(500).send(err)
+        }
+    )
+})
 app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on Port ${process.env.PORT}`);
+    console.log(`Server is running on Port ${port}`);
 })
